ece1a01031e36e3dc653bacca32acc8f332936dc
Tiziano Lattisi <tiziano@axiastudio.it>
2013-04-03 14:20:29 +0200

    Corretto un piccolo errore di visualizzazione del Logger.

--- src/com/axiastudio/pypapi/plugins/jente/JEnteHelper.java
+++ src/com/axiastudio/pypapi/plugins/jente/JEnteHelper.java
@@ -61,7 +61,9 @@ public class JEnteHelper {
                 return res.getRispostaEsisteBozzaOAtto().isEsiste();
             } else {
                 Logger.getLogger(JEnteHelper.class.getName()).log(Level.SEVERE, "Consultazione fallita: {0}", res.getMessage());
-                Logger.getLogger(JEnteHelper.class.getName()).log(Level.SEVERE, "Consultazione fallita: {0}", res.getRispostaElencoMovimenti().getMessage());
+                if( res.getRispostaElencoMovimenti() != null ){
+                    Logger.getLogger(JEnteHelper.class.getName()).log(Level.SEVERE, "Consultazione fallita: {0}", res.getRispostaElencoMovimenti().getMessage());
+                }
             }
         } catch (Exception ex) {
             Logger.getLogger(JEnteHelper.class.getName()).log(Level.SEVERE, null, ex);
